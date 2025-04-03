e646cfa1fd08dc38aa3be447cb00c43f5387e544
TeredBite <Guzenko.step@yandex.ru>
2025-04-03 23:02:26 +0300

    .

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,5 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 #include <Windows.h>
@@ -8,85 +9,95 @@ using namespace std;
 
 bool CheckBinary(string numbers)
 {
-	bool space = false;
-	int size = numbers.size();
-	for (int i = 0; i < size; i++)
-	{
-		if (numbers[i] != ' ')
-		{
-			space = true;
-			break;
-		}
-	}
-	if (!space)
-	{
-		return false;
-	}
-	for (int i = 0; i < size; i++)
-	{
-		if ((numbers[i] != '1') && (numbers[i] != '0') && (numbers[i] != ' ')) {
-			return false;
-		}
-	}
-	return true;
+    bool space = false;
+    int size = numbers.size();
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] != ' ')
+        {
+            space = true;
+            break;
+        }
+    }
+    if (!space)
+    {
+        return false;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if ((numbers[i] != '1') && (numbers[i] != '0') && (numbers[i] != ' ')) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int choice;
-	cout << "Выберите номер действие: " << endl 
-        << "1 - Первое задание." << endl 
-        << "2 - Второе задание." 
-        << endl << "3 - Третье задание." 
-        << endl << "4 - Выход из программы." << endl;
-	cin >> choice;
-	cin.ignore();
-	switch (choice)
-	{
-	case (1):
-	{
-		string line;
-		cout << "Введите строку:";
-		getline(cin, line);
-		bool check = CheckBinary(line);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int choice;
+    cout << "Выберите номер действия: " << endl
+        << "1 - Первое задание." << endl
+        << "2 - Второе задание." << endl
+        << "3 - Третье задание." << endl
+        << "4 - Выход из программы." << endl;
+    cin >> choice;
+    cin.ignore();
 
-		while (!check) 
-		{
-			cout << "Ошибка ввода. Введите корректную строку.";
-			getline(cin, line);
-			check = CheckBinary(line);
-		}
+    switch (choice)
+    {
+    case (1):
+    {
+        string line;
+        cout << "Введите строку: ";
+        getline(cin, line);
+        bool check = CheckBinary(line);
 
-		cout << "Введена корректная строка: " << line << endl;
+        while (!check)
+        {
+            cout << "Ошибка ввода. Введите корректную строку." << endl;
+            getline(cin, line);
+            check = CheckBinary(line);
+        }
 
-		stringstream ss(line);
-		string group;
-		cout << "Группы с чётным количеством символов:" << endl;
-		while (ss >> group)
-		{
-			if (group.size() % 2 == 0)
-			{
-				cout << group << endl;
-			}
-		}
-		break;
-	}
+        cout << "Введена корректная строка: " << line << endl;
 
+        stringstream ss(line);
+        string group;
+        cout << "Группы с чётным количеством символов:" << endl;
+        while (ss >> group)
+        {
+            if (group.size() % 2 == 0)
+            {
+                cout << group << endl;
+            }
+        }
+        break;
+    }
     case (2):
     {
-        int methodChoice;
         cout << "Выберите способ обработки строки:" << endl;
         cout << "1 - std::string\n2 - C-строки (массив символов)" << endl;
+        int methodChoice;
         cin >> methodChoice;
         cin.ignore();
 
         if (methodChoice == 1) // string
         {
-            cout << "Введите строку, где слова разделены пробелами и символами '/':" << endl;
+            string filename;
+            cout << "Введите название файла: ";
+            cin >> filename;
+            cin.ignore();
+
+            ifstream file(filename);
+            if (!file) {
+                cerr << "Ошибка открытия файла!" << endl;
+                exit(1);
+            }
             string line;
-            getline(cin, line);
+            getline(file, line);
+            file.close();
 
             for (char& ch : line)
             {
@@ -111,7 +122,6 @@ int main()
             }
 
             string* words = new string[wordCount];
-
             istringstream iss2(line);
             int index = 0;
             while (iss2 >> temp)
@@ -120,66 +130,37 @@ int main()
                 index++;
             }
 
-            int count3 = 0;
-            cout << "\nСлова длиной 3 символа:" << endl;
+            cout << "Слова длиной 3 символа:" << endl;
             for (int i = 0; i < wordCount; i++)
             {
                 if (words[i].size() == 3)
                 {
                     cout << words[i] << endl;
-                    count3++;
                 }
             }
 
-            if (count3 == 0)
-            {
-                cout << "Нет слов длиной 3 символа." << endl;
-            }
-            else
-            {
-                cout << "Количество слов длиной 3 символа: " << count3 << endl;
-            }
-
-            int totalLength = 0;
-            for (int i = 0; i < wordCount; i++)
-            {
-                totalLength += words[i].size();
-            }
-
-            double avgLength = static_cast<double>(totalLength) / wordCount;
-            cout << "Средняя длина слов: " << avgLength << endl;
-
-            bool found = false;
-            cout << "Слова средней длины:" << endl;
-            for (int i = 0; i < wordCount; i++)
-            {
-                if (words[i].size() == static_cast<int>(avgLength))
-                {
-                    cout << words[i] << endl;
-                    found = true;
-                }
-            }
-
-            if (!found)
-            {
-                cout << "Таких слов нет." << endl;
-            }
-
-            cout << "Обратный порядок:" << endl;
-            for (int i = wordCount - 1; i >= 0; i--)
-            {
-                cout << words[i] << " ";
-            }
-            cout << endl;
             delete[] words;
         }
-        else if (methodChoice == 2) // Работа с C-строками
+        else if (methodChoice == 2) // C-строки
         {
-            cout << "Введите строку (макс. 256 символов), где слова разделены пробелами и символами '/':" << endl;
-            char line[256];
-            cin.getline(line, 256);
+            string filename;
+            cout << "Введите название файла: ";
+            cin >> filename;
+            cin.ignore();
 
-            // Замена '/' на пробелы
+            ifstream file(filename);
+            if (!file) {
+                cerr << "Ошибка открытия файла!" << endl;
+                exit(1);
+            }
+            string fileContent;
+            getline(file, fileContent);
+            file.close();
+
+            char line[256];
+            strncpy(line, fileContent.c_str(), 255);
+            line[255] = '\0';
+
             for (int i = 0; line[i] != '\0'; i++)
             {
                 if (line[i] == '/')
@@ -190,7 +171,6 @@ int main()
 
             char* words[50];
             int wordCount = 0;
-
             char* token = strtok(line, " ");
             while (token != nullptr)
             {
@@ -199,63 +179,14 @@ int main()
                 token = strtok(nullptr, " ");
             }
 
-            if (wordCount == 0)
-            {
-                cout << "В строке нет слов." << endl;
-                return 0;
-            }
-
-            int count3 = 0;
-            cout << "\nСлова длиной 3 символа:" << endl;
+            cout << "Слова длиной 3 символа:" << endl;
             for (int i = 0; i < wordCount; i++)
             {
                 if (strlen(words[i]) == 3)
                 {
                     cout << words[i] << endl;
-                    count3++;
                 }
             }
-
-            if (count3 == 0)
-            {
-                cout << "Нет слов длиной 3 символа." << endl;
-            }
-            else
-            {
-                cout << "Количество слов длиной 3 символа: " << count3 << endl;
-            }
-
-            int totalLength = 0;
-            for (int i = 0; i < wordCount; i++)
-            {
-                totalLength += strlen(words[i]);
-            }
-
-            double avgLength = static_cast<double>(totalLength) / wordCount;
-            cout << "Средняя длина слов: " << avgLength << endl;
-
-            bool found = false;
-            cout << "Слова средней длины:" << endl;
-            for (int i = 0; i < wordCount; i++)
-            {
-                if (strlen(words[i]) == static_cast<int>(avgLength))
-                {
-                    cout << words[i] << endl;
-                    found = true;
-                }
-            }
-
-            if (!found)
-            {
-                cout << "Таких слов нет." << endl;
-            }
-
-            cout << "Обратный порядок:" << endl;
-            for (int i = wordCount - 1; i >= 0; i--)
-            {
-                cout << words[i] << " ";
-            }
-            cout << endl;
         }
         else
         {
@@ -263,10 +194,38 @@ int main()
         }
         break;
     }
+    case (3):
+    {
+        string filename;
+        cout << "Введите название файла: ";
+        cin >> filename;
+        cin.ignore();
 
-	default:
-	{
-		cout << "Неправильный выбор. Введите число от 1 до 4." << endl;
-	}
-	}
+        ifstream file(filename);
+        if (!file) {
+            cerr << "Ошибка открытия файла!" << endl;
+            exit(1);
+        }
+
+        string word;
+        int count = 0;
+        while (file >> word)
+        {
+            if (word.length() == 5)
+            {
+                count++;
+            }
+        }
+        file.close();
+
+        cout << "Количество слов из пяти букв: " << count << endl;
+        break;
+    }
+    case (4):
+    {
+        break;
+    }
+    default:
+        cout << "Неправильный выбор. Введите число от 1 до 4." << endl;
+    }
 }
